358066cb734bfec92f6e2df6ec03eca16ac9acd8
jadenPete <jadenpete@github.com>
2024-02-11 17:09:03 -0500

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import daisyui from "daisyui";
+
+export default {
+	content: ["src/**/*.{html,svelte,ts}"],
+	theme: {
+		extend: {
+			colors: {
+				"calendar-card-1": "#040b10",
+				"calendar-card-2": "#9ac3db",
+				"calendar-card-3": "#ad99ac",
+				"calendar-card-4": "#33c95d",
+				"calendar-card-5": "#c02626",
+				"calendar-card-6": "#eaebeb",
+				"calendar-card-7": "#bababa"
+			}
+		}
+	}
+}
