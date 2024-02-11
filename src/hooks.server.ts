3318a885341433a332e8a74161633e3dd5eeb853
DylanHalstead <dylanhalstead11@gmail.com>
2024-02-10 19:17:27 -0500

    removing logs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,5 @@
 import { SvelteGoogleAuthHook } from 'svelte-google-auth/server';
-import { redirect, type Handle } from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 
 // Import client credentials from json file
 import client_secret from '../client_secret.json';
@@ -7,6 +7,5 @@ import client_secret from '../client_secret.json';
 const auth = new SvelteGoogleAuthHook(client_secret.web);
 
 export const handle: Handle = async ({ event, resolve }) => {
-	
 	return await auth.handleAuth({ event, resolve });
 };
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -8,19 +8,13 @@ export const load: LayoutServerLoad = async ({ locals }) => {
 	let db: {
 		user: User;
 	} | null = null;
-  console.log("hello world")
 	if (!locals.db && isSignedIn(locals)) {
-    console.log('User is signed in');
 		if (await doesUserExist(locals.user.email)) {
-      console.log('User exists');
       db = {user: await getUserByEmail(locals.user.email)};
     } else {
-      console.log('User does not exist');
       if (!locals.user.email.endsWith('@uncc.edu') && !locals.user.email.endsWith('@charlotte.edu')) {
-        console.log('User does not have a valid email');
         redirect(307, '_auth/signout')
       } else {
-        console.log('Creating user');
         db = {user: await createUser(locals.user.email, locals.user.given_name, locals.user.family_name)};
       }
     }
