4ade1784ed7bee259277fff7979be5653c461d43
kfinberg7581 <kfinberg7581@gmail.com>
2024-02-13 12:16:05 -0500

    updated routes with slugs where needed

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/">Dashboard</a>
+    <a href="/profile/[id]">Profile</a>
+    <a href="/calendar">Calendar</a>
+    <a href="/courses/[id]/sections/[id]/members/[id]/appointments/book">Book</a>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>ClassConnect Landing Page / Dashboard :D</h1>
+
+<p>I think I did the routing correctly pt.2</p>
+
+<a href="/calendar">Calendar</a>
+<a href="/profile">Profile</a>
+<a href="/courses/[id]/sections/[id]/members/[id]/appointments/book">Book</a>
