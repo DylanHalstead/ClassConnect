e5cd7ead4b4d0705d7afca7c781a07a6ea8c145b
James Anderson <j.anderson2429@gmail.com>
2024-02-27 13:49:50 -0500

    added frontend of Dashboard with dynamic dates

---
+++ src/lib/components/dashboard/BookingForm.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+	// Blockers 
+	// booking form missing time selector
+	// Or a pop up for picking a time for that specified TA.
+
+	let classes = [
+		{
+			id: 1,
+			text: "Course #1"
+		},
+		{
+			id: 2,
+			text: "Course #2"
+		},
+		{
+			id: 3,
+			text: "Course #3"
+		},
+		{
+			id: 4,
+			text: "Course #4"
+		},
+		{
+			id: 5,
+			text: "Course #5"
+		}
+	];
+
+	let selectedClass: any;
+	let chosenClass = "";
+
+	let tas = [
+		{
+			id: 1,
+			text: "TA Example 1"
+		},
+		{
+			id: 2,
+			text: "TA Example 2"
+		},
+		{
+			id: 3,
+			text: "TA Example 3"
+		}
+	];
+
+	let selectedTA: any;
+	let chosenTA = "";
+</script>
+
+<h2 class="subheading main-text text-center">Book Now</h2>
+<form action="" class="form-control w-80">
+	<label for="name" class="main-text mt-4">Name:</label>
+	<input
+		type="text"
+		class="input-sm rounded-md bg-gray-200 input-bordered w-full max-w-xs text-black mt-1"
+		placeholder="John Doe" />
+
+	<label for="email" class="main-text mt-4">Email:</label>
+	<input
+		type="text"
+		class="input-sm rounded-md bg-gray-200 input-bordered w-full max-w-xs text-black mt-1"
+		placeholder="example@uncc.edu" />
+
+	<label for="date" class="main-text mt-4">Date:</label>
+	<input
+		type="date"
+		class="input-sm rounded-md bg-gray-200 input-bordered w-full max-w-xs text-black mt-1" />
+
+	<label for="class" class="main-text mt-4">Course:</label>
+	<select
+		name="courses"
+		id="courses"
+		class="input-sm rounded-md bg-gray-200 input-bordered w-full max-w-xs text-black mt-1"
+		value={selectedClass}
+		on:change={() => chosenClass}>
+		{#each classes as course}
+			<option value={course}>
+				{course.text}
+			</option>
+		{/each}
+	</select>
+
+	<label for="ta" class="main-text mt-4">TA:</label>
+	<select
+		name="ta"
+		id="ta"
+		class="input-sm rounded-md bg-gray-200 input-bordered w-full max-w-xs text-black mt-1"
+		value={selectedTA}
+		on:change={() => chosenTA}>
+		{#each tas as ta}
+			<option value={ta}>
+				{ta.text}
+			</option>
+		{/each}
+	</select>
+
+	<button class="btn btn-primary btn-sm text-base-100 main-text mt-4 mx-20 rounded-md">
+		Submit
+	</button>
+</form>
+
+<style>
+	.main-text {
+		font-family: "Kaisei HarunoUmi", serif;
+		font-size: 1rem;
+	}
+
+	.subheading {
+		font-weight: bold;
+		font-size: 1.75rem;
+	}
+</style>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,131 +1,86 @@
+<script lang="ts">
+	import BookingForm from "$lib/components/dashboard/BookingForm.svelte";
+	import OfficeHourSummary from "$lib/components/dashboard/OfficeHourSummary.svelte";
+
+	const currentDate = new Date();
+	const dayNumber = currentDate.getDay();
+	const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	const DayOfWeek = weekdays[dayNumber];
+
+	const month = currentDate.toLocaleString("en-US", {
+		month: "long"
+	});
+
+	const day = currentDate.toLocaleString("en-US", {
+		day: "numeric"
+	});
+
+	const year = currentDate.toLocaleString("en-US", {
+		year: "numeric"
+	});
+
+	function getNumSuffix(i: number): string {
+		let selected;
+
+		if ((i > 3 && i < 21) || i % 10 > 3) {
+			selected = 0;
+		} else {
+			selected = i % 10;
+		}
+
+		return ["th", "st", "nd", "rd"][selected];
+	}
+
+	const dateSuffix = getNumSuffix(currentDate.getDate());
+</script>
+
 <div class="px-12 py-2">
-    <div class="flex flex-row w-full">
-        <div class ="flex justify-between w-full">
-            <h1 class ="main-text header"> Monday, January 29<sup class="text-primary">th</sup></h1>
-            <h1 class ="main-text header-year text-primary px-8"> 2024 </h1>
-        </div> 
-    </div>
+	<div class="flex flex-row w-full">
+		<div class="flex justify-between w-full">
+			<h1 class="main-text header">
+				{DayOfWeek}, {month}
+				{day}<sup class="text-primary">{dateSuffix}</sup>
+			</h1>
+			<h1 class="main-text header-year text-primary px-8">{year}</h1>
+		</div>
+	</div>
 
-    <div class="border-t-4 border-solid border-gray-500 my-4 me-32"></div>
-    <div class="px-12 py-6 mx-auto flex flex-row">
-        <div>
-            <h2 class="subheading main-text"> Upcoming Appointments </h2>
-            <div class="px-12">
-                <p class="main-text pt-6">Today:<p>
-                <div class= "flex flex-row py-4 items-center">
-                    <div class="h-7 w-7 bg-calendar-card-2 rounded-lg"></div>
-                    <div class="px-2">
-                        <p class="main-text">Office Hours with Jaden</p>
-                        <p class="main-text subtext">Thursday, January 5th</p>
-                        <p class="main-text subtext">10:30-11:00 AM</p>
-                    </div>
-                </div>
-            </div>
-            <div class="px-12">
-                <p class="main-text pt-6">Tomorrow:<p>
-                <div class= "flex flex-row py-4 items-center">
-                    <div class="h-7 w-7 bg-calendar-card-2 rounded-lg"></div>
-                    <div class="px-4">
-                        <p class="main-text">Office Hours with Jaden</p>
-                        <p class="main-text subtext">Thursday, January 5th</p>
-                        <p class="main-text subtext">10:30-11:00 AM</p>
-                    </div>
-                </div>
+	<div class="border-t-4 border-solid border-gray-500 my-4 me-36"></div>
 
-                <div class= "flex flex-row py-4 items-center">
-                    <div class="h-7 w-7 bg-calendar-card-2 rounded-lg"></div>
-                    <div class="px-4">
-                        <p class="main-text">Office Hours with Jaden</p>
-                        <p class="main-text subtext">Thursday, January 5th</p>
-                        <p class="main-text subtext">10:30-11:00 AM</p>
-                    </div>
-                </div>
-            </div>
-            <div class="mt-12">
-                <h2 class="subheading main-text"> TA Meetings: </h2>
-                <div class="px-12">
-                    <p class="main-text pt-6">Today:<p>
-                    <div class= "flex flex-row py-4 items-center">
-                        <div class="h-7 w-7 bg-calendar-card-3 rounded-lg"></div>
-                        <div class="px-4">
-                            <p class="main-text">Office Hours with Dylan</p>
-                            <p class="main-text subtext">Thursday, January 5th</p>
-                            <p class="main-text subtext">10:30-11:00 AM</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="flex-1 border-r border-gray-300"></div>
-        <div>
-            <h2 class="subheading main-text"> Upcoming Appointments </h2>
-            <div class="px-12">
-                <p class="main-text pt-6">Today:<p>
-                <div class= "flex flex-row py-4 items-center">
-                    <div class="h-7 w-7 bg-calendar-card-2 rounded-lg"></div>
-                    <div class="px-2">
-                        <p class="main-text">Office Hours with Jaden</p>
-                        <p class="main-text subtext">Thursday, January 5th</p>
-                        <p class="main-text subtext">10:30-11:00 AM</p>
-                    </div>
-                </div>
-            </div>
-            <div class="px-12">
-                <p class="main-text pt-6">Tomorrow:<p>
-                <div class= "flex flex-row py-4 items-center">
-                    <div class="h-7 w-7 bg-calendar-card-2 rounded-lg"></div>
-                    <div class="px-4">
-                        <p class="main-text">Office Hours with Jaden</p>
-                        <p class="main-text subtext">Thursday, January 5th</p>
-                        <p class="main-text subtext">10:30-11:00 AM</p>
-                    </div>
-                </div>
+	<div class="px-12 py-6 mx-auto flex flex-row justify-center">
+		<div class="appointment my-8 mx-auto">
+			<h2 class="subheading main-text">Upcoming Appointments</h2>
+			<OfficeHourSummary />
+			<OfficeHourSummary />
+			<OfficeHourSummary />
+			<div class="mt-12">
+				<h2 class="subheading main-text">TA Meetings:</h2>
+				<OfficeHourSummary />
+			</div>
+		</div>
 
-                <div class= "flex flex-row py-4 items-center">
-                    <div class="h-7 w-7 bg-calendar-card-2 rounded-lg"></div>
-                    <div class="px-4">
-                        <p class="main-text">Office Hours with Jaden</p>
-                        <p class="main-text subtext">Thursday, January 5th</p>
-                        <p class="main-text subtext">10:30-11:00 AM</p>
-                    </div>
-                </div>
-            </div>
-            <div class="mt-12">
-                <h2 class="subheading main-text"> TA Meetings: </h2>
-                <div class="px-12">
-                    <p class="main-text pt-6">Today:<p>
-                    <div class= "flex flex-row py-4 items-center">
-                        <div class="h-7 w-7 bg-calendar-card-3 rounded-lg"></div>
-                        <div class="px-4">
-                            <p class="main-text">Office Hours with Dylan</p>
-                            <p class="main-text subtext">Thursday, January 5th</p>
-                            <p class="main-text subtext">10:30-11:00 AM</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+		<div class="divider divider-horizontal my-16 mx-auto"></div>
+
+		<div class="booking form px-6 pt-24 flex flex-col mx-auto">
+			<BookingForm />
+		</div>
+	</div>
 </div>
 
 <style>
-    .main-text {
-        font-family: 'Kaisei HarunoUmi', serif;
-        font-size: 1rem;
-    }
+	.main-text {
+		font-family: "Kaisei HarunoUmi", serif;
+		font-size: 1rem;
+	}
 
-    .header, .header-year {
+	.header,
+	.header-year {
 		font-weight: bold;
-        font-size: 2.5rem;
-    }
+		font-size: 2.5rem;
+	}
 
-    .subheading {
+	.subheading {
 		font-weight: bold;
-        font-size: 1.75rem;
-    }
-
-    .subtext {
-        font-size: 0.75rem;
-    }
+		font-size: 1.75rem;
+	}
 </style>
-
