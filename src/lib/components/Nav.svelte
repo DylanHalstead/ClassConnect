957820e7091dc2347d953c9e5922e035cb767d59
kfinberg7581 <kfinberg7581@users.noreply.github.com>
2024-03-19 21:25:24 +0000

    fixup! Formatted code

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { signOut } from "svelte-google-auth/client";
-    import { Icon, User, Calendar, BookOpen, ArrowLeftEndOnRectangle } from "svelte-hero-icons";
+	import { Icon, User, Calendar, BookOpen, ArrowLeftEndOnRectangle } from "svelte-hero-icons";
 
-    import { invalidateAll } from "$app/navigation";
+	import { invalidateAll } from "$app/navigation";
 	import { initialize } from "svelte-google-auth/client";
 	import type { PageData } from "./$types.js";
-    
+
 	export let data: PageData;
 	initialize(data, invalidateAll);
 </script>
@@ -18,39 +18,40 @@
 					<a class="text-2xl font-bold" href="/">CC</a>
 				</div>
 			</div>
-        </nav>
-    </div>
+		</nav>
+	</div>
 
-    <div>
-        <nav class="flex flex-col items-center space-y-10">
-            <div class="bg-white w-3/5 m-5 h-12 rounded-xl flex items-center justify-center">
-                <div>
-                    <!-- If not user show CC 'logo' -->
-                    {#if !data.auth.user}
-                        <a class="text-2xl font-bold" href="/">CC</a>
-                    <!-- Else show CC user pfp -->
-                    {:else}
-                        <img src={data.auth.user.picture}>
-                    {/if}
-                </div>
-            </div>
+	<div>
+		<nav class="flex flex-col items-center space-y-10">
+			<div class="bg-white w-3/5 m-5 h-12 rounded-xl flex items-center justify-center">
+				<div>
+					<!-- If not user show CC 'logo' -->
+					{#if !data.auth.user}
+						<a class="text-2xl font-bold" href="/">CC</a>
+						<!-- Else show CC user pfp -->
+					{:else}
+						<img src={data.auth.user.picture} />
+					{/if}
+				</div>
+			</div>
 
-            <div class="space-y-14">
-                <div>
-                    <a href="/profile/[profileID]"><Icon src="{User}" size="32" class="text-white" /></a>
-                </div>
-                <div>
-                    <a href="/calendar"><Icon src="{Calendar}" size="32" class="text-white" /></a>
-                </div>
-                <div>
-                    <a href="/courses/[courseID]/sections/[sectionID]/members/[memberID]/appointments/book"><Icon src="{BookOpen}" size="32" class="text-white" /></a>
-                </div>
-            </div>  
-        </nav>
-    </div>
+			<div class="space-y-14">
+				<div>
+					<a href="/profile/[profileID]"><Icon src={User} size="32" class="text-white" /></a>
+				</div>
+				<div>
+					<a href="/calendar"><Icon src={Calendar} size="32" class="text-white" /></a>
+				</div>
+				<div>
+					<a href="/courses/[courseID]/sections/[sectionID]/members/[memberID]/appointments/book"
+						><Icon src={BookOpen} size="32" class="text-white" /></a>
+				</div>
+			</div>
+		</nav>
+	</div>
 
-    <div class="mb-5">
-        <button on:click={() => signOut()}><Icon src="{ArrowLeftEndOnRectangle}" size="32" class="text-white" /></button>
-    </div>
-
+	<div class="mb-5">
+		<button on:click={() => signOut()}
+			><Icon src={ArrowLeftEndOnRectangle} size="32" class="text-white" /></button>
+	</div>
 </div>
--- src/routes/profile/[profileID]/+page.svelte
+++ src/routes/profile/[profileID]/+page.svelte
@@ -3,4 +3,3 @@
 <a href="/dashboard">Dashboard</a>
 <a href="/calendar">Calendar</a>
 <a href="/courses/[id]/sections/[id]/members/[id]/appointments/book">Book</a>
-
