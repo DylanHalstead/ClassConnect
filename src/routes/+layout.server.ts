f1b992b66c79eb1bee1ab636a6b0f5bf0083c718
DylanHalstead <dylanhalstead11@gmail.com>
2024-03-21 14:44:16 -0400

    login logic refactor

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -7,41 +7,40 @@ import type { LayoutServerLoad } from "./$types";
 export const load: LayoutServerLoad = loadFlash(async ({ locals, cookies, fetch }) => {
 	let db: {
 		user: User;
-	} | null = null;
+	} | undefined = undefined;
 	// If the user has signed in with google but hasn't grabbed their data from the database
 	if (!locals.db && isSignedIn(locals)) {
-		const user = await getUserByEmail(locals.user.email);
+		let user = await getUserByEmail(locals.user.email);
+		let messageType = "";
+		let messageContent = "";
 		if (user) {
-			db = { user: user };
-			cookies.set("userID", db.user.id, { path: "/" });
-			const message = {
-				type: "success",
-				message: "Logged in successfully!"
-			} as const;
-			setFlash(message, cookies);
+			messageType = "success";
+			messageContent = "Logged in successfully!";
 		} else {
 			if (
 				!locals.user.email.endsWith("@uncc.edu") &&
 				!locals.user.email.endsWith("@charlotte.edu")
 			) {
 				await fetch("/_auth/signout");
-				const message = {
-					type: "error",
-					message: "You must use a UNC Charlotte email address to sign in."
-				} as const;
-				setFlash(message, cookies);
+				messageType = "error"
+				messageContent = "You must use a UNC Charlotte email address to sign in."
 			} else {
-				db = {
-					user: await createUser(locals.user.email, locals.user.given_name, locals.user.family_name)
-				};
-				cookies.set("userID", db.user.id, { path: "/" });
-				const message = {
-					type: "success",
-					message: "Created account successfully!"
-				} as const;
-				setFlash(message, cookies);
+				user = await createUser(locals.user.email, locals.user.given_name, locals.user.family_name)
+				messageType = "success";
+				messageContent = "Created account successfully!";
 			}
 		}
+		if (user) {
+			db = {
+				user: user,
+			};
+			cookies.set("userID", db.user.id, { path: "/" });
+		}
+		const message = {
+			type: messageType,
+			message: messageContent
+		} as const;
+		setFlash(message, cookies);
 	}
 
 	return { ...hydrateAuth(locals), db };
