3261ae67af40fbc798230fec69ff8afaa34ac899
jadenPete <jadenpete@github.com>
2024-03-26 13:22:17 -0400

    Wrote functions to create appointment blocks, appointments, section members, sections, and users

---
+++ src/lib/db/sections.ts
@@ -0,0 +1,30 @@
+import type { QueryConfig } from "pg";
+import { v4 as uuidv4 } from "uuid";
+import type { PartialSection, Section } from "$lib/types";
+import { withConnection } from ".";
+
+export async function createSection(partialSection: PartialSection): Promise<Section> {
+	return withConnection(async client => {
+		const newSection: Section = {
+			...partialSection,
+
+			id: uuidv4()
+		};
+
+		const query: QueryConfig = {
+			text: `
+INSERT INTO sections (id, course_id, section_number, max_daily_bookable_hours)
+VALUES ($1, $2, $3, $4)`,
+			values: [
+				newSection.id,
+				newSection.course_id,
+				newSection.section_number,
+				newSection.max_daily_bookable_hours
+			]
+		};
+
+		await client.query(query);
+
+		return newSection;
+	});
+}
