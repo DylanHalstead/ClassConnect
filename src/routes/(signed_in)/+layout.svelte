13ff5e2eae6f429871b0f21377c47b360f2f64cf
Dylan Jacob Halstead <Dylanhalstead11@gmail.com>
2024-03-28 15:29:41 -0400

    Quality of Life: Favicon, Readme, Navbar On Certain Pages (#53)
    * favicon update
    
    * revamped readme
    
    * fixup! Formatted code
    
    * all emojis same side
    
    ---------
    
    Co-authored-by: DylanHalstead <DylanHalstead@users.noreply.github.com>

---
+++ src/routes/(signed_in)/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { getFlash } from "sveltekit-flash-message";
+	import { page } from "$app/stores";
+	import "../app.css";
+	import Nav from "$lib/components/Nav.svelte";
+	import { invalidateAll } from "$app/navigation";
+	import { initialize } from "svelte-google-auth/client";
+
+	import type { PageData } from "./$types.js";
+
+	export let data: PageData;
+	initialize(data, invalidateAll);
+
+	const flash = getFlash(page, {
+		clearAfterMs: 5000
+	});
+</script>
+
+<div class="flex">
+	<Nav data="data" />
+
+	<div class="flex-1 ml-16">
+		{#if $flash}
+			<p>{$flash.message}</p>
+		{/if}
+
+		<slot />
+	</div>
+</div>
