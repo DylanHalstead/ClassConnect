1eab6a8c70e8d133f5c26963ed3dc06939095843
DylanHalstead <dylanhalstead11@gmail.com>
2024-03-29 16:44:57 -0400

    auth on page load

--- src/routes/(signed_in)/courses/[courseID]/sections/[sectionID]/+page.server.ts
+++ src/routes/(signed_in)/courses/[courseID]/sections/[sectionID]/+page.server.ts
@@ -1,10 +1,17 @@
 import type { PageServerLoad, Actions } from './$types';
 import { getExtendedSectionMembers } from '$lib/db/sectionMembers';
 import { getExtendedSection } from '$lib/db/section';
+import { verifyAuthentication, verifyUserIsApartOfInstructionalTeam } from '$lib/auth';
 import { error } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ locals, cookies, params }) => {
 	const { sectionID } = params;
+  verifyAuthentication(locals, cookies);
+  const userID: string | undefined = cookies.get("userID");
+  if (!userID) {
+    error(401, "You must be signed in to view this page.");
+  }
+  await verifyUserIsApartOfInstructionalTeam(cookies, userID, sectionID)
   let section = await getExtendedSection(sectionID);
   let sectionMembers = await getExtendedSectionMembers(sectionID);
   if (!section) {
