de405af0b42b7ebd7349e33b0d6dbf19a18bbbe4
Dylan Jacob Halstead <Dylanhalstead11@gmail.com>
2024-04-08 19:03:34 -0400

    Popup Notifications (#56)
    * popup styling
    
    * remove test flash message
    
    * magic number removal
    
    * semantic tag
    
    * fixup! Formatted code
    
    * refactor of unnecessary class concatination abstraction
    
    * fixup! Formatted code
    
    * refactor layouts
    
    * fixup! Formatted code
    
    * removed line breaks
    
    ---------
    
    Co-authored-by: DylanHalstead <DylanHalstead@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
 	import { initialize } from "svelte-google-auth/client";
-	import { getFlash } from "sveltekit-flash-message";
 	import { invalidateAll } from "$app/navigation";
-	import { page } from "$app/stores";
+	import Notification from "$lib/components/Notification.svelte";
 	import type { PageData } from "./$types.js";
 	import "../app.css";
 
 	export let data: PageData;
 	initialize(data, invalidateAll);
-
-	const flash = getFlash(page, {
-		clearAfterMs: 5000
-	});
 </script>
 
-{#if $flash}
-	<p>{$flash.message}</p>
-{/if}
-
+<Notification />
 <slot />
