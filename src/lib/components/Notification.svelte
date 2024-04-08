2ac297093f1d7ac8b6a76c13c522ede06a44a76b
DylanHalstead <dylanhalstead11@gmail.com>
2024-04-08 15:23:15 -0400

    refactor of unnecessary class concatination abstraction

--- src/lib/components/Notification.svelte
+++ src/lib/components/Notification.svelte
@@ -3,7 +3,6 @@
 	import { page } from "$app/stores";
 	import { slide } from "svelte/transition";
 	import { quintInOut } from "svelte/easing";
-	import { cn } from "$lib/utils";
 	import { Icon, CheckCircle, ExclamationCircle, InformationCircle } from "svelte-hero-icons";
 
 	const flash = getFlash(page);
@@ -44,10 +43,8 @@
 {#if isVisible && $flash}
 	<dialog
 		transition:slide={{ delay: 100, duration: 200, easing: quintInOut, axis: "y" }}
-		class={cn(
-			"flex items-center justify-center w-fit backdrop-blur-md rounded-md text-sm p-1 fixed m-auto left-0 right-0 top-4 z-50 border-2",
-			currentType.classNames
-		)}>
+		class={
+			`flex items-center justify-center w-fit backdrop-blur-md rounded-md text-sm p-1 fixed m-auto left-0 right-0 top-4 z-50 border-2 ${currentType.classNames}`}>
 		<Icon src={currentType.icon} class="w-6 h-6" />
 		<p class="ml-2 mr-1">{$flash.message}</p>
 		<button on:click={() => (isVisible = false)} class="btn btn-sm btn-circle btn-ghost">✕</button>
