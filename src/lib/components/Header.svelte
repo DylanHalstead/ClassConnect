6302dd01a8ae6b32ef2ab5455551ab4d186ea38a
Sm3ggl3s <Sm3ggl3s@users.noreply.github.com>
2024-04-09 23:18:14 +0000

    fixup! Formatted code

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -40,38 +40,37 @@
 	}
 
 	const dateSuffix = getNumSuffix(currentDate.getDate());
-	
+
 	export let layout: string;
 </script>
 
 <div>
-    <div class="px-12 py-2">
-		{#if layout == 'dashboard'}
-		<div class="header-component">
-			<div class="flex flex-row w-full">
-				<div class="flex justify-between w-full">
-					<h1 class="main-text header capitalize">
-						{DayOfWeek}, {month}
-						{day}<sup class="text-primary">{dateSuffix}</sup>
-					</h1>
-					<h1 class="main-text header-year text-primary px-8">{year}</h1>
+	<div class="px-12 py-2">
+		{#if layout == "dashboard"}
+			<div class="header-component">
+				<div class="flex flex-row w-full">
+					<div class="flex justify-between w-full">
+						<h1 class="main-text header capitalize">
+							{DayOfWeek}, {month}
+							{day}<sup class="text-primary">{dateSuffix}</sup>
+						</h1>
+						<h1 class="main-text header-year text-primary px-8">{year}</h1>
+					</div>
 				</div>
 			</div>
-		</div>
-		{:else if layout === 'calendar'}
-		<div class="flex flex-row justify-between">
-			
-			<div class="flex flex-col items-start">
-				<h1 class="header-year font-bold font-kaisei">
-					<span>{month}</span> <span class="text-primary">{year}</span>
-				</h1>
-				<h2 class="subheading font-bold font-kaisei text-primary text-xl">{month}/{day}</h2>
+		{:else if layout === "calendar"}
+			<div class="flex flex-row justify-between">
+				<div class="flex flex-col items-start">
+					<h1 class="header-year font-bold font-kaisei">
+						<span>{month}</span> <span class="text-primary">{year}</span>
+					</h1>
+					<h2 class="subheading font-bold font-kaisei text-primary text-xl">{month}/{day}</h2>
+				</div>
+				<slot />
 			</div>
-			<slot></slot>
-		</div>
 		{/if}
-        <div class="border-t-4 border-solid border-gray-500 my-4 me-36"></div>
-    </div>
+		<div class="border-t-4 border-solid border-gray-500 my-4 me-36"></div>
+	</div>
 </div>
 
 <style>
