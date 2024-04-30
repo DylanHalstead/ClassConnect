1d15fdfced7da63d42c5e60104535b0297bedabc
DylanHalstead <dylanhalstead11@gmail.com>
2024-04-30 12:14:18 -0400

    dyanamic title functionalty

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,19 @@
 	import { invalidateAll } from "$app/navigation";
 	import { initialize } from "svelte-google-auth/client";
 	import type { LayoutData } from "./$types.js";
+	import { title } from "$lib/stores";
 	import Notification from "$lib/components/Notification.svelte";
-	import "../app.css";
 
+	title.clear();
+	
 	export let data: LayoutData;
 	initialize(data, invalidateAll);
 </script>
+
+
+<svelte:head>
+	<title>{$title}</title>
+</svelte:head>
 
 <Notification />
 <slot />
