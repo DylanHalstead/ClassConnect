f6b2e8edc613ee7d27682063c8986b1e2d223938
DylanHalstead <DylanHalstead@users.noreply.github.com>
2024-05-02 19:07:45 +0000

    fixup! Formatted code

--- src/lib/components/profile/Appointment.svelte
+++ src/lib/components/profile/Appointment.svelte
@@ -1,36 +1,33 @@
 <script lang="ts">
+	import type { ExtendedAppointment } from "$lib/types";
+	import { sectionName, userName } from "$lib/utils";
 
-    import type { ExtendedAppointment } from "$lib/types";
-    import { sectionName, userName } from "$lib/utils";
+	export let appointment: ExtendedAppointment;
 
-    export let appointment: ExtendedAppointment;
-
-    $: appointmentBlock = appointment.appointment_block;
+	$: appointmentBlock = appointment.appointment_block;
 	$: instructionalMember = appointmentBlock.instructional_member;
 	$: startTime = appointment.appointment_block.start_time;
+</script>
 
- </script>
- 
- <!-- Appointment Space -->
- <div class="flex space-x-5">
+<!-- Appointment Space -->
+<div class="flex space-x-5">
+	<!-- Appointment Block -->
+	<div class="bg-secondary w-fit h-fit p-4 rounded">
+		<!-- Name of TA -->
+		<p class="font-bold mb-1">Office Hours with {userName(instructionalMember.user)}</p>
 
-    <!-- Appointment Block -->
-    <div class="bg-secondary w-fit h-fit p-4 rounded">
+		<!-- Class of Appointment -->
+		<p class="text-sm">{sectionName(instructionalMember.section)}</p>
 
-        <!-- Name of TA -->
-        <p class='font-bold mb-1'>Office Hours with {userName(instructionalMember.user)}</p>
+		<!-- Time of Appointment -->
+		<p class="text-sm">
+			{startTime.toLocaleString("en-US", {
+				timeStyle: "short"
+			})}
+		</p>
 
-        <!-- Class of Appointment -->
-        <p class="text-sm">{sectionName(instructionalMember.section)}</p>
-
-        <!-- Time of Appointment -->
-        <p class="text-sm">{startTime.toLocaleString("en-US", {
-            timeStyle: "short"
-        })}</p>
-
-        <!-- Unique Zoom Link -->
-        <a class="text-sm" href={appointment.link}><u>{appointment.link}</u></a>
-
-    </div>
+		<!-- Unique Zoom Link -->
+		<a class="text-sm" href={appointment.link}><u>{appointment.link}</u></a>
+	</div>
 </div>
-    <!-- End Appointment Block -->
+<!-- End Appointment Block -->
--- src/lib/components/profile/InstructorClass.svelte
+++ src/lib/components/profile/InstructorClass.svelte
@@ -1,39 +1,33 @@
 <script lang="ts">
+	// We want this to display the classes in which an instructor teaches only
+	// Instead of a booking button in the modal, point to the class overview page
+	import type { PartialCourse } from "$lib/types";
 
-// We want this to display the classes in which an instructor teaches only
-// Instead of a booking button in the modal, point to the class overview page
-import type { PartialCourse } from "$lib/types";
+	export let insCourse: PartialCourse;
 
-export let insCourse : PartialCourse;
-
-$: name = insCourse.course_name;
-$: dptName = insCourse.department_code;
-
+	$: name = insCourse.course_name;
+	$: dptName = insCourse.department_code;
 </script>
 
 <!-- Class Space -->
 <div class="flex flex-wrap space-x-5">
+	<!-- Class Block -->
+	<div class="bg-neutral w-fit h-fit p-4 rounded flex justify-between space-x-5">
+		<!-- Dynamic: Name of Class and its Department Code -->
+		<div class="flex space-x-2">
+			<div>
+				<p class="p-1">{dptName}</p>
+			</div>
+			<div>
+				<p class="p-1">{name}</p>
+			</div>
+		</div>
 
-    <!-- Class Block -->
-    <div class="bg-neutral w-fit h-fit p-4 rounded flex justify-between space-x-5">
-
-        <!-- Dynamic: Name of Class and its Department Code -->
-        <div class="flex space-x-2">
-            <div>
-                <p class="p-1">{dptName}</p>
-            </div>
-            <div>
-                <p class="p-1">{name}</p>
-            </div>
-        </div>
-
-        <div class="flex rounded shadow-md p-1 bg-secondary pl-2 pr-2">
-
-            <div>
-                <a class="text-white" href="/courses/[id]/sections/[id]">View Class</a>
-            </div>
-        </div>
-
-    </div>
+		<div class="flex rounded shadow-md p-1 bg-secondary pl-2 pr-2">
+			<div>
+				<a class="text-white" href="/courses/[id]/sections/[id]">View Class</a>
+			</div>
+		</div>
+	</div>
 </div>
-    <!-- End Class Block -->
+<!-- End Class Block -->
